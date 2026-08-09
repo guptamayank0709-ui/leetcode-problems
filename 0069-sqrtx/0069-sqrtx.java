18ff81e49ec304ce94ac3ebd65bfe8aab371405a
mayank gupta <guptamayank0709@gmail.com>
2026-08-09 19:30:04 +0530

    Time: 1 ms (97.81%), Space: 42.6 MB (43.5%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+    long start = 0;
+    long end = x;
+    
+    while(start<=end){
+        long mid = start+(end-start)/2;
+        if(mid*mid ==x){
+            return (int)(mid);
+        }else if(mid*mid<x){
+            start =(int)(mid+1);
+        }else{
+            end = (int)(mid-1);
+        }
+
+    }
+    return (int)(end);
+
+    }
+}
